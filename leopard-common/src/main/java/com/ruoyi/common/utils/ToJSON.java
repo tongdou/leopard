b926ca306c6b40ee96c85986fbfdd18fc1ec9003
pleuvoir <pleuvoir@foxmail.com>
2022-02-12 14:17:05 +0800

    init

---
+++ leopard-common/src/main/java/com/ruoyi/common/utils/ToJSON.java
@@ -0,0 +1,13 @@
+package com.ruoyi.common.utils;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * @author <a href="mailto:fuwei13@xdf.cn">pleuvoir</a>
+ */
+public interface ToJSON {
+
+    default String ToJSON() {
+        return JSON.toJSONString(this);
+    }
+}
